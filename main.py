776801a043d35ea6de1e5084ff79cf52a975e486
omkar3311 <omkarwaghmare3311@gmail.com>
2026-02-04 16:41:39 +0530

    appti - questions

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,JSONResponse
+import pandas as pd
+from app import AI_que ,AI_res
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
-
-QUESTION = "What is the capital of France?"
-CORRECT_ANSWER = "paris"
-
+# @app.get("/", response_class=HTMLResponse)
+# def home(request: Request):
+#     return templates.TemplateResponse(
+#         "index.html",
+#         {"request": request}
+#     )
 @app.get("/question")
 def get_que():
-    return {"question": QUESTION}
+    question = AI_que()
+    return {"question": question}
+
 
 @app.post("/evaluate")
 async def evaluate_answer(request: Request):
     body = await request.json()
+
+    question = body.get("question", "")
     answer = body.get("answer", "").strip().lower()
 
-    if not answer:
+    if not question or not answer:
         return {
             "correct": False,
             "score": 0,
-            "feedback": "No answer provided"
+            "feedback": "Missing question or answer"
         }
 
-    if answer == CORRECT_ANSWER:
-        return {
-            "correct": True,
-            "score": 1,
-            "feedback": "Correct answer. Well done."
-        }
+    ai_result = AI_res(question, answer)
+
+    return {"evaluation": ai_result}
+
+
+CSV_PATH = "data.csv"
+df = pd.read_csv(CSV_PATH , delimiter= ';')
+df.columns = df.columns.str.strip()
+
+@app.get("/")
+def home(request: Request):
+    types = sorted(df["Type"].dropna().unique().tolist())
+    return templates.TemplateResponse(
+        "appti.html",
+        {"request": request, "types": types}
+    )
+
+@app.get("/questions/{qtype}")
+def get_questions(qtype: str):
+    filtered_df = df[df["Type"] == qtype]
+
+    questions = filtered_df.to_dict(orient="records")
+    return JSONResponse(questions)
+
+@app.post("/check-answer")
+async def check_answer(payload: dict):
+    user_ans = payload["user_answer"]
+    correct_ans = payload["correct_answer"]
+    approach = payload["approach"]
+
+    is_correct = user_ans == correct_ans
 
     return {
-        "correct": False,
-        "score": 0,
-        "feedback": "Incorrect. The correct answer is Paris."
+        "correct": is_correct,
+        "approach": approach if not is_correct else None
     }
