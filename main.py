c22e9f37220e5d3caf71e32960ef0b2acdad3926
omkar3311 <omkarwaghmare3311@gmail.com>
2026-02-07 09:46:10 +0530

    commented appti and use hardcored que insted ai

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse,JSONResponse
 import pandas as pd
 from app import AI_que ,AI_res
-
+from questions import que
+import random
+import time
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-# @app.get("/", response_class=HTMLResponse)
-# def home(request: Request):
-#     return templates.TemplateResponse(
-#         "index.html",
-#         {"request": request}
-#     )
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
 @app.get("/question")
 def get_que():
-    question = AI_que()
+    question = random.choice(list(que.keys()))
     return {"question": question}
-
 
 @app.post("/evaluate")
 async def evaluate_answer(request: Request):
@@ -33,40 +34,46 @@ async def evaluate_answer(request: Request):
             "score": 0,
             "feedback": "Missing question or answer"
         }
-
+    start_time = time.perf_counter()
     ai_result = AI_res(question, answer)
+    end_time = time.perf_counter()
 
-    return {"evaluation": ai_result}
-
-
-CSV_PATH = "data.csv"
-df = pd.read_csv(CSV_PATH , delimiter= ';')
-df.columns = df.columns.str.strip()
-
-@app.get("/")
-def home(request: Request):
-    types = sorted(df["Type"].dropna().unique().tolist())
-    return templates.TemplateResponse(
-        "appti.html",
-        {"request": request, "types": types}
-    )
-
-@app.get("/questions/{qtype}")
-def get_questions(qtype: str):
-    filtered_df = df[df["Type"] == qtype]
-
-    questions = filtered_df.to_dict(orient="records")
-    return JSONResponse(questions)
-
-@app.post("/check-answer")
-async def check_answer(payload: dict):
-    user_ans = payload["user_answer"]
-    correct_ans = payload["correct_answer"]
-    approach = payload["approach"]
-
-    is_correct = user_ans == correct_ans
+    total_time = round(end_time - start_time, 2)
 
     return {
-        "correct": is_correct,
-        "approach": approach if not is_correct else None
+        "evaluation": ai_result,
+        "time_taken": total_time
     }
+
+
+# CSV_PATH = "data.csv"
+# df = pd.read_csv(CSV_PATH , delimiter= ';')
+# df.columns = df.columns.str.strip()
+
+# @app.get("/")
+# def home(request: Request):
+#     types = sorted(df["Type"].dropna().unique().tolist())
+#     return templates.TemplateResponse(
+#         "appti.html",
+#         {"request": request, "types": types}
+#     )
+
+# @app.get("/questions/{qtype}")
+# def get_questions(qtype: str):
+#     filtered_df = df[df["Type"] == qtype]
+
+#     questions = filtered_df.to_dict(orient="records")
+#     return JSONResponse(questions)
+
+# @app.post("/check-answer")
+# async def check_answer(payload: dict):
+#     user_ans = payload["user_answer"]
+#     correct_ans = payload["correct_answer"]
+#     approach = payload["approach"]
+
+#     is_correct = user_ans == correct_ans
+
+#     return {
+#         "correct": is_correct,
+#         "approach": approach if not is_correct else None
+#     }
